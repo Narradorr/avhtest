1d9bf4ef4863861b5587307dbe4277623bbb7959
Азиз Гайсин <gaysin.aziz@gmail.com>
2020-11-07 15:59:28 +0600

    initialize

---
+++ src/components/Seed.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="seed">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Seed",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style >
+</style>
---
+++ src/components/Structure.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="structure page">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Structure',
+  props: {
+    
+  }
+}
+</script>
+
+<style >
+
+</style>
